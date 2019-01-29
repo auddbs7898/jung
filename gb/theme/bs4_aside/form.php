dbb57d487cf0389edac6dde662f9a8fc52185ff3
auddbs7898 <dpdms7898@naver.com>
2019-01-29 14:30:33 +0900

    다시정리

---
+++ gb/theme/bs4_aside/form.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+<style>
+body,ul,li,div,p{ margin:0; padding:0}
+</style>
+</head>
+
+<body>
+<form action="wright.php" method="post">
+ <ul>
+  <li><span>이름</span><input type="text" name="wr_name"></li>
+  <li><span>제목</span><input type="text" name="wr_subject"></li>
+  <li><span>내용</span><input type="text" name="wr_content"></li>
+ </ul>
+ <input type="submit" value="글쓰기">
+</form>
+</body>
+</html>
